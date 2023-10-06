4983d40a5db05d6ed4adac5a6e41827877496ce1
GhostWisher13 <birdrang13@gmail.com>
2023-10-06 16:13:46 -0300

    inicio do projeto :D

---
+++ teste.php
@@ -0,0 +1,5 @@
+<?php
+echo "<center><h1>Página gerada no servidor Srv5997-0080</h1></center>";
+echo "<center><h1>Seja bem-vindo a página local do servidor WEB; Instalado por - 2021325997</h1></center><br> <h1>2020330080</h1><br>";
+phpinfo();
+?>
